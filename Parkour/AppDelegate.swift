773522356b4ecadcd1b8879483d9dcf8b6272f15
tushar1210 <ts26192271@gmail.com>
2019-10-10 02:08:48 +0530

    UI Added

--- Parkour/AppDelegate.swift
+++ Parkour/AppDelegate.swift
@@ -5,7 +5,7 @@
 //  Created by Tushar Singh on 13/09/19.
 //  Copyright © 2019 Tushar Singh. All rights reserved.
 //
-
+import Firebase
 import UIKit
 
 @UIApplicationMain
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
