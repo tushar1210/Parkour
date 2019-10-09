30558e973633383a8ff0167d0362213142d99b75
tushar1210 <ts26192271@gmail.com>
2019-10-10 00:07:57 +0530

    Initial

---
+++ Parkour/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Parkour
+//
+//  Created by Tushar Singh on 13/09/19.
+//  Copyright © 2019 Tushar Singh. All rights reserved.
+//
+
+import UIKit
+import Lottie
+class ViewController: UIViewController {
+
+    
+let animView = AnimationView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        animView.frame = CGRect(x: 67, y: 110, width: 240, height: 206)
+        view.addSubview(animView)
+        let anim = Animation.named("3532-car")
+        animView.animation=anim
+        animView.play()
+        animView.loopMode = .loop
+    }
+
+
+    @IBAction func loginButton(_ sender: Any) {
+    }
+    
+    @IBAction func signUpButton(_ sender: Any) {
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Parkour' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Parkour
+
+pod 'Firebase/Core'
+pod 'Firebase/Database'
+pod 'SwiftyJSON'
+pod 'Alamofire'
+pod 'lottie-ios'
+end
